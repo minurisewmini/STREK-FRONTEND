2fec6d505d23558ae032c4ae42b2b4ba4f8fbbfc
Minuri Sewmini <minurisewmini11@gmail.com>
2025-08-09 12:59:31 +0530

    install axios

--- src/pages/admin/adminProduct.jsx
+++ src/pages/admin/adminProduct.jsx
@@ -1,4 +1,14 @@
+import axios from "axios"
+
 export default function AdminProductsPage(){
+
+axios.get("http://localhost:5000/api/products").then((res)=>{
+    console.log(res)
+})
+
+
+
+
    return(
     <div>
         <h1>Admin Product Page</h1>
